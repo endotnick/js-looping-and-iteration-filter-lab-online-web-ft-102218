979dc2016aef9754b10f58dae90d9fa141ad7fc2
endotnick <endotnick@gmail.com>
2019-02-04 15:17:11 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (name) { return name.toLowerCase() === string.toLowerCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (name) { return name.startsWith(string); });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (element) { return element.name.toLowerCase() === string.toLowerCase(); });
+}
